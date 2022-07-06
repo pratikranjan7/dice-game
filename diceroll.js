45a1457ddb91b9a503a017d44f9add87b5f91f0b
Pratik Ranjan <108509145+pratikranjan7@users.noreply.github.com>
2022-07-06 22:39:41 +0530

    initial commit

---
+++ diceroll.js
@@ -0,0 +1,35 @@
+var player1 = 0
+var player2 = 0
+
+var turn = 1
+
+var target = 30
+
+function roll() {
+    var num = getRndInteger(1, 6);
+    document.getElementById('val3').innerText = ' ' + num;
+    if (turn === 1) {
+        player1 = player1 + num;
+        turn = 2;
+        document.getElementById('val1').innerText = ' ' + player1;
+        if (player1 >= target) {
+            document.getElementById('val4').innerText = 'Player1 Wins!';
+            setTimeout(() => { window.location.reload(); }, 3000);
+            console.log("got it!");
+        }
+    }
+    else if (turn === 2) {
+        player2 = player2 + num;
+        turn = 1;
+        document.getElementById('val2').innerText = ' ' + player2;
+        if (player2 >= target) {
+            document.getElementById('val4').innerText = 'Player2 Wins!';
+            setTimeout(() => { window.location.reload(); }, 3000);
+            console.log("got it!");
+        }
+    }
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
